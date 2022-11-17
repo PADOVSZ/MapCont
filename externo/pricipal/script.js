d5fe0578506d3be895f05d1b92aa09edde43c224
Beatriz Coutinho <beatrizjcoutinho@gmail.com>
2022-11-17 10:53:43 -0300

    atualizacoes na pagina principal

--- externo/pricipal/script.js
+++ externo/pricipal/script.js
@@ -1,13 +1,19 @@
 function Mostrar(id) {
     let info = document.querySelector(id)
     let button = document.getElementsByClassName("btn")
+
     info.style.display = "block"
-    button.style.display = "none"
+
+    for (let i = 0; i < button.length; i++)
+        button[i].style.display = "none"
 }
 
 function Fechar(id) {
     let info = document.querySelector(id)
     let button = document.getElementsByClassName('btn')
+
     info.style.display = "none"
-    button.style.display = "block"
+
+    for (let i = 0; i < button.length; i++)
+        button[i].style.display = "block"
 }
