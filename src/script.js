913be4160a0c8053bba1fffa82556f8ad81e7dc0
BEATRIZ JULIATO COUTINHO <u22121@academico.cotuca.unicamp.br>
2022-11-17 08:10:57 -0300

    .

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function Mostrar(id) {
+    let info = document.querySelector(id)
+    let button = document.getElementsByClassName('btn')
+    info.style.display = "block"
+    button.style.display = "none"
+}
+
+function Fechar(id) {
+    let info = document.querySelector(id)
+    info.style.display = "none"
+}
