b5848fd87ffe0aee835d0356408d1686cfa110bb
Miojodetomat <cc22136@g.unicamp.br>
2022-12-04 21:16:12 -0300

    Rotas dos cards prontas

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -14,3 +14,35 @@ exports.principal = ('/principal', async(req, res) => {
         })
     })
 })
+
+exports.cards = ('/principal/americadosulcard', async(req, res) => {
+    res.render('./cards/Cards_AmericaDoSul/Card_AmericaDoSul.html')
+})
+
+exports.cards = ('/principal/americadonortecard', async(req, res) => {
+    res.render('./cards/Cards_AmericaDoNorte/Card_AmericaDoNorte.html')
+})
+
+exports.cards = ('/principal/americacentralcard', async(req, res) => {
+    res.render('./cards/Cards_AmericaCentral/Card_AmericaCentral.html')
+})
+
+exports.cards = ('/principal/asiacard', async(req, res) => {
+    res.render('./cards/cards_Asia/card_Asia.html')
+})
+
+exports.cards = ('/principal/africacard', async(req, res) => {
+    res.render('./cards/cards_Africa/card_Africa.html')
+})
+
+exports.cards = ('/principal/europacard', async(req, res) => {
+    res.render('./cards/cards_Europa/card_Europa.html')
+})
+
+exports.cards = ('/principal/antartidacard', async(req, res) => {
+    res.render('./cards/cards_Antartida/card_Antartida.html')
+})
+
+exports.cards = ('/principal/oceaniacard', async(req, res) => {
+    res.render('./cards/cards_Oceania/card_Oceania.html')
+})
